49c3d47f75016359a5a6d79de6e5e59c222d8560
Alex Mihailov <av.mihailov.dev@gmail.com>
2023-06-27 20:17:56 +0300

    Generate `plugin.xml` stub

--- src/main/scala/com/github/sbt/maven/SbtMavenPlugin.scala
+++ src/main/scala/com/github/sbt/maven/SbtMavenPlugin.scala
@@ -4,9 +4,28 @@
 
 package com.github.sbt.maven
 
-import sbt.AutoPlugin
+import java.io.File
+
+import sbt.*
+import sbt.librarymanagement.Configurations.Compile
+import sbt.Keys.*
 
 object SbtMavenPlugin extends AutoPlugin {
 
   override def requires = sbt.plugins.JvmPlugin
+
+  override lazy val projectSettings: Seq[Setting[?]] = inConfig(Compile)(mavenPluginSettings)
+
+  private def mavenPluginSettings: Seq[Setting[?]] = Seq(
+    Compile / resourceGenerators += generateMavenPluginXml.taskValue
+  )
+
+  private val generateMavenPluginXml = Def.task {
+    compile.value // generation must be started after compilation
+    val pluginXML = (Compile / resourceManaged).value / "META-INF" / "maven" / "plugin.xml"
+    // TODO: implement
+    IO.write(pluginXML, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+    Seq(pluginXML)
+  }
+
 }
